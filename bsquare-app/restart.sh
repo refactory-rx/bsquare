6c087e7bdbf0f3204f9628b7be4fd85ff74f3820
Vladimir Halme <vhalme@gmail.com>
2015-07-08 12:39:28 +0200

    Changed restart script

--- bsquare-app/restart.sh
+++ bsquare-app/restart.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-echo Stopping app
-forever stop bsquare-app
 echo Pulling latest from master
 git pull
-echo Waiting 8 secs and restarting
-sleep 8
-npm run babel && npm run forever
+echo Transpiling ES6 client code
+npm run babel
+echo Restarting server
+pkill -f "bsquare-app/server.js"
