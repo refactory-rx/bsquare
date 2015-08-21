ff8bc2453e3fab816405843c73996f5d2e543afe
Vladimir Halme <vhalme@gmail.com>
2015-08-21 15:03:38 +0200

    Added reinstallation to retsrat script

--- bsquare-app/restart.sh
+++ bsquare-app/restart.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 echo Pulling latest from master
 git pull
+echo Reinstall npm
+npm install
+echo Reinstall bower
+bower install
+echo Reinstall tsd
+tsd install
 echo Transpiling ES6 client code
 grunt babel ts concat ngAnnotate uglify:server
 echo Restarting server
