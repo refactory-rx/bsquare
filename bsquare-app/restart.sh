f3ca26fba035cc9ddf5ba28528840583d4eb675e
Vladimir Halme <vhalme@gmail.com>
2015-08-21 14:54:58 +0200

    Added ts grunt task to restar script

--- bsquare-app/restart.sh
+++ bsquare-app/restart.sh
@@ -2,6 +2,6 @@
 echo Pulling latest from master
 git pull
 echo Transpiling ES6 client code
-grunt babel concat ngAnnotate uglify:server
+grunt babel ts concat ngAnnotate uglify:server
 echo Restarting server
 pkill -f "bsquare-app/server.js"
