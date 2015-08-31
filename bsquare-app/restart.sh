b89fd822477e644855ca049e292b43e8892b6120
Vladimir Halme <vhalme@gmail.com>
2015-08-31 15:39:21 +0200

    Removed ng2 from restart script

--- bsquare-app/restart.sh
+++ bsquare-app/restart.sh
@@ -5,9 +5,9 @@ echo Reinstall npm
 npm install
 echo Reinstall bower
 bower install
-echo Reinstall tsd
-tsd install
+# echo Reinstall tsd
+# tsd install
 echo Transpiling ES6 client code
-grunt babel ts concat ngAnnotate uglify:server
+grunt babel concat ngAnnotate uglify:server
 echo Restarting server
 pkill -f "bsquare-app/server.js"
